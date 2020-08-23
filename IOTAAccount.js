a4e0521e97c28cfe03d155848caf3bda3609ef48
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-23 21:38:21 +0300

    IOTA API

---
+++ IOTAAccount.js
@@ -0,0 +1,17 @@
+const Iota = require('@iota/core`);
+
+var settings = {network: "http-client", provider: "http://localhost:14265", attachToTangle: "attachToTangle", apiVersion = 1, requestBatchSize = 1000}
+   
+var iota = Iota.core.composeApi(settings);
+
+var address = "";
+
+getBalances([address])
+  .then( balances => {
+    console.log(`Balance of the first address: `$balances.balances[0]);
+    console.log(JSON.stringify(transactions));
+  });
+  .catch(error => {
+    console.log(`Something went wrong: ${error}`);
+};
+
