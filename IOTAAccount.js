aa4fb9e2569c0baf901f723b8218db72637c9741
Tomi Toivio <tomi.toivio@ukuli.fi>
2020-08-23 21:39:27 +0300

    IOTA API

--- IOTAAccount.js
+++ IOTAAccount.js
@@ -6,7 +6,7 @@ var iota = Iota.core.composeApi(settings);
 
 var address = "";
 
-getBalances([address])
+iota.getBalances([address])
   .then( balances => {
     console.log(`Balance of the first address: `$balances.balances[0]);
     console.log(JSON.stringify(transactions));
